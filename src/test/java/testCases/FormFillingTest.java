351b1e2707bcf98e64138be8af2a6b0820c7732f
mustafaOzsamur <mustafaozsamur22@gmail.com>
2023-04-03 02:50:46 +0200

    FormFillingTest

---
+++ src/test/java/testCases/FormFillingTest.java
@@ -0,0 +1,60 @@
+package testCases;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import utilities.ReusableMethods;
+import utilities.TestBase;
+
+public class FormFillingTest extends TestBase {
+    @Test
+    public void test01(){
+        // go to facebook.com
+        driver.get("https://www.facebook.com");
+        // accept cookies
+        driver.findElement(By.xpath("//button[@title='Nur erforderliche Cookies erlauben']")).click();
+        // click the create new account button
+        driver.findElement(By.xpath("//a[@class='_42ft _4jy0 _6lti _4jy6 _4jy2 selected _51sy']")).click();
+
+        // fill relevant fields with values from faker library
+        WebElement firstnameKutusu=driver.findElement(By.name("firstname"));
+
+        String mailAdresi=faker.internet().emailAddress();
+
+        actions.click(firstnameKutusu)
+                .sendKeys(faker.name().firstName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.name().lastName())
+                .sendKeys(Keys.TAB)
+                .sendKeys(mailAdresi)
+                .sendKeys(Keys.TAB)
+                .sendKeys(mailAdresi)
+                .sendKeys(Keys.TAB)
+                .sendKeys(faker.internet().password())
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys("23")
+                .sendKeys(Keys.TAB)
+                .sendKeys("Oct")
+                .sendKeys(Keys.TAB)
+                .sendKeys("1990")
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.TAB)
+                .sendKeys(Keys.ARROW_RIGHT).perform();
+
+        // press the register button
+        WebElement signupButonu= driver.findElement(By.name("websubmit"));
+        signupButonu.click();
+        // test that you are not registered
+
+        ReusableMethods.bekle(20);
+        WebElement hataMesaji=driver.findElement(By.xpath("//span[text()='Wir benötigen noch weitere Informationen']"));
+
+        Assert.assertTrue(hataMesaji.isDisplayed());
+
+
+
+    }
+}
--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -1,18 +1,24 @@
 package utilities;
 
+import com.github.javafaker.Faker;
 import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 import java.time.Duration;
 
 public class TestBase {
 
     protected WebDriver driver;
+    protected Actions actions;
+    protected Faker faker;
     @Before
     public void setUp(){
         driver=new ChromeDriver();
+        faker=new Faker();
+        actions=new Actions(driver);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
     }
